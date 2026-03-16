ed29bd9b993d9f098582014ae336f74ec6da510d
sima00rnmr <sima92rnmr@gmail.com>
2026-03-16 19:25:29 +0900

    13章末まで完了_ただしUIがおかしいので確認の必要あり　①追加登録したホテルの画像が表示されない②indexのフッターの位置がおかしい

--- moattravel_3s/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel_3s/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -6,22 +6,30 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel.entity.House;
 import com.example.moattravel.form.HouseRegisterForm;
 import com.example.moattravel.repository.HouseRepository;
+import com.example.moattravel.service.HouseService;
 
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
 	private final HouseRepository houseRepository;
+	private final HouseService houseService;
 
-	public AdminHouseController(HouseRepository houseRepository) {
+	public AdminHouseController(HouseRepository houseRepository,HouseService houseService) {
 		this.houseRepository = houseRepository;
+		this.houseService = houseService;
 
 	}
 
@@ -60,5 +68,15 @@ public class AdminHouseController {
 		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
 		return "admin/houses/register";
 		
+	}
+	@PostMapping("/create")
+	public String create(@ModelAttribute @Validated HouseRegisterForm houseRegisterForm,BindingResult bindingResult,RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			return "admin/houses/register";
+		}
+		houseService.create(houseRegisterForm);
+		redirectAttributes.addFlashAttribute("successMessage","民宿を登録しました。");
+		
+	return "redirect:/admin/houses";
 	}
 }
