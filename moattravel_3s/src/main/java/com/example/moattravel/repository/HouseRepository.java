b824b135894e6c0ae889c8606d0626fe44d4a109
sima00rnmr <sima92rnmr@gmail.com>
2026-03-18 19:24:05 +0900

    28_3まで　完了

--- moattravel_3s/src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ moattravel_3s/src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -1,13 +1,10 @@
 package com.example.moattravel.repository;
 
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel.entity.House;
-
-
 
 /*JpaRepositoryインターフェースを継承すると…
  * findAll()：テーブル内のすべてのエンティティを取得
@@ -19,12 +16,33 @@ import com.example.moattravel.entity.House;
  * 等の機能が利用可能になる
  * */
 
-public interface HouseRepository extends JpaRepository<House,Integer>{
+public interface HouseRepository extends JpaRepository<House, Integer> {
 	//findBy○○○Like()というメソッドで
 	//SQLのLIKE句と同様のクエリを実行できる
-	public Page<House>findByNameLike(String keyword,Pageable pageable);
-	
-	public Page<House>findByNameLikeOrAddressLike(String nameKeyword,String addressKeyword,Pageable pageable);
-	public Page<House>findByAddressLike(String area,Pageable pageable);
-	public Page<House>findByPriceLessThanEqual(Integer price,Pageable pageable);
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
+
+	public Page<House> findByAddressLike(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByCreatedAtDesc(String area, Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByPriceAsc(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable);
+
+	public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+	public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
+
 }
