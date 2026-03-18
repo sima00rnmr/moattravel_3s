791583aadfa23afa77b78cbab9553195ba28ac1d
sima00rnmr <sima92rnmr@gmail.com>
2026-03-18 14:10:53 +0900

    25章終了

--- moattravel_3s/src/main/java/com/example/moattravel/service/UserService.java
+++ moattravel_3s/src/main/java/com/example/moattravel/service/UserService.java
@@ -17,65 +17,88 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
 @Service
-public class UserService{
-private final UserRepository userRepository;
-private final RoleRepository roleRepository;
-private final PasswordEncoder passwordEncoder;
+public class UserService {
+	private final UserRepository userRepository;
+	private final RoleRepository roleRepository;
+	private final PasswordEncoder passwordEncoder;
 
-public UserService(UserRepository userRepository,RoleRepository roleRepository,PasswordEncoder passwordEncoder) {
-	this.userRepository = userRepository;
-	this.roleRepository =roleRepository;
-	this.passwordEncoder =passwordEncoder;
-}
-@Transactional
-public User create(SignupForm signupForm) {
-	User user =new User();
-	Role role = roleRepository.findByName("ROLE_GENERAL");
-	
-	user.setName(signupForm.getName());
-	user.setFurigana(signupForm.getFurigana());
-	user.setPostalCode(signupForm.getPostalCode());
-	user.setAddress(signupForm.getAddress());
-	user.setPhoneNumber(signupForm.getPhoneNumber());
-	user.setEmail(signupForm.getEmail());
-	user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
-	user.setRole(role);
-	user.setEnabled(false);
-	
-	return userRepository.save(user);
-	
-}
-/*以降…　登録時にメールアドレスが既に登録済みかどうか
- *をチェックする機能をつける
- * */
+	public UserService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-//メールアドレスが登録済みかどうかをチェックする
-public boolean isEmailRegistered(String email) {
-	//もし、Userのアドレス（このアドレスを持っているユーザーがいるかどうか）をみつけることが出来たら…nullで返す（アドレスの重複を許容しない）
-	User user = userRepository.findByEmail(email);
-	return user !=null;
-	
-}
+	@Transactional
+	public User create(SignupForm signupForm) {
+		User user = new User();
+		Role role = roleRepository.findByName("ROLE_GENERAL");
 
-//パスワードとパスワード（確認用）の入力値が一致するかどうかチェックする
-public boolean isSamePassword(String password,String passwordConfirmation) {
-	return password.equals(passwordConfirmation);
-}
+		user.setName(signupForm.getName());
+		user.setFurigana(signupForm.getFurigana());
+		user.setPostalCode(signupForm.getPostalCode());
+		user.setAddress(signupForm.getAddress());
+		user.setPhoneNumber(signupForm.getPhoneNumber());
+		user.setEmail(signupForm.getEmail());
+		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
+		user.setRole(role);
+		user.setEnabled(false);
 
-//ユーザーを有効にする
-/*認証に成功した際に実行するメソッド
- * 
- * */
-@Transactional
-public void enableUser(User user) {
-	user.setEnabled(true);
-	userRepository.save(user);
-	
-	
-}
+		return userRepository.save(user);
+
+	}
+
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+		User user = userRepository.getReferenceById(userEditForm.getId());
+
+		user.setName(userEditForm.getName());
+		user.setFurigana(userEditForm.getFurigana());
+		user.setPostalCode(userEditForm.getPostalCode());
+		user.setAddress(userEditForm.getAddress());
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+		user.setEmail(userEditForm.getEmail());
+
+		userRepository.save(user);
+
+	}
+
+	/*以降…　登録時にメールアドレスが既に登録済みかどうか
+	 *をチェックする機能をつける
+	 * */
+
+	//メールアドレスが登録済みかどうかをチェックする
+	public boolean isEmailRegistered(String email) {
+		//もし、Userのアドレス（このアドレスを持っているユーザーがいるかどうか）をみつけることが出来たら…nullで返す（アドレスの重複を許容しない）
+		User user = userRepository.findByEmail(email);
+		return user != null;
+
+	}
+
+	//パスワードとパスワード（確認用）の入力値が一致するかどうかチェックする
+	public boolean isSamePassword(String password, String passwordConfirmation) {
+		return password.equals(passwordConfirmation);
+	}
+
+	//ユーザーを有効にする
+	/*認証に成功した際に実行するメソッド
+	 * 
+	 * */
+	@Transactional
+	public void enableUser(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
+
+	}
+
+	//メールアドレスが変更されたかどうかチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
+	}
 
 }
