0a86308a886cca62a2f2e25b6dc7cccedb79f1eb
sima00rnmr <sima92rnmr@gmail.com>
2026-03-23 11:24:48 +0900

    34章終了

--- moattravel_3s/src/main/java/com/example/moattravel/controller/ReservationController.java
+++ moattravel_3s/src/main/java/com/example/moattravel/controller/ReservationController.java
@@ -1,5 +1,7 @@
 package com.example.moattravel.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -7,27 +9,97 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.moattravel.entity.House;
 import com.example.moattravel.entity.Reservation;
 import com.example.moattravel.entity.User;
+import com.example.moattravel.form.ReservationInputForm;
+import com.example.moattravel.form.ReservationRegisterForm;
+import com.example.moattravel.repository.HouseRepository;
 import com.example.moattravel.repository.ReservationRepository;
 import com.example.moattravel.security.UserDetailsImpl;
+import com.example.moattravel.service.ReservationService;
 
 @Controller
 public class ReservationController {
 	private final ReservationRepository reservationRepository;
+	private final HouseRepository houseRepository;
+	private final ReservationService reservationService;
 
-	public ReservationController(ReservationRepository reservationRepository) {        
+	public ReservationController(ReservationRepository reservationRepository, HouseRepository houseRepository,
+			ReservationService reservationService) {
 		this.reservationRepository = reservationRepository;
+		this.houseRepository = houseRepository;
+		this.reservationService = reservationService;
 	}
+
 	@GetMapping("/reservations")
-	public String index(@AuthenticationPrincipal  UserDetailsImpl userDetailsImpl,@PageableDefault(page =0,size =10,sort="id",direction =Direction.ASC)Pageable pageable,Model model) {
+	public String index(@AuthenticationPrincipal UserDetailsImpl userDetailsImpl,
+			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+			Model model) {
 		User user = userDetailsImpl.getUser();
-		Page<Reservation>reservationPage = reservationRepository.findByUserOrderByCreatedAtDesc(user,pageable);
-		model.addAttribute("reservationPage",reservationPage);
-		
+		Page<Reservation> reservationPage = reservationRepository.findByUserOrderByCreatedAtDesc(user, pageable);
+		model.addAttribute("reservationPage", reservationPage);
+
 		return "reservations/index";
+	}
+
+	@GetMapping("/houses/{id}/reservations/input")
+	public String input(@PathVariable(name = "id") Integer id,
+			@ModelAttribute @Validated ReservationInputForm reservationInputForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes, Model model) {
+		House house = houseRepository.getReferenceById(id);
+		Integer capacity = house.getCapacity();
+		Integer numberOfPeople = reservationInputForm.getNumberOfPeople();
+
+		if (numberOfPeople != null) {
+			if (!reservationService.isWithinCapacity(numberOfPeople, capacity)) {
+				FieldError fieldError = new FieldError(bindingResult.getObjectName(), "numberOfPeople",
+						"宿泊人数が定員を超えています。");
+				bindingResult.addError(fieldError);
+			}
+		}
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("house", house);
+			model.addAttribute("errorMessage", "予約内容に不備があります");
+			return "houses/show";
+
+		}
+		redirectAttributes.addFlashAttribute("reservationInputForm", reservationInputForm);
+
+		return "redirect:/houses/{id}/reservations/confirm";
+	}
+
+	@GetMapping("/houses/{id}/reservations/confirm")
+	public String confim(@PathVariable(name = "id") Integer id,
+			@ModelAttribute ReservationInputForm reservationInputForm,
+			@AuthenticationPrincipal UserDetailsImpl userDetailImpl, Model model) {
+		House house = houseRepository.getReferenceById(id);
+		User user = userDetailImpl.getUser();
+
+		//チェックイン日とチェックアウト日を取得する
+		LocalDate checkinDate = reservationInputForm.getCheckinDate();
+		LocalDate checkoutDate = reservationInputForm.getCheckoutDate();
+
+		//宿泊料金を計算する
+
+		Integer price = house.getPrice();
+		Integer amount = reservationService.calculateAmount(checkinDate, checkoutDate, price);
+
+		ReservationRegisterForm reservationRegisterForm = new ReservationRegisterForm(house.getId(), user.getId(),
+				checkinDate.toString(), checkoutDate.toString(), reservationInputForm.getNumberOfPeople(), amount);
+
+		model.addAttribute("house", house);
+		model.addAttribute("reservationRegisterForm", reservationRegisterForm);
+		return "reservations/confirm";
+
 	}
 
 }
--- moattravel_3s/src/main/java/com/example/moattravel/form/ReservationInputForm.java
+++ moattravel_3s/src/main/java/com/example/moattravel/form/ReservationInputForm.java
@@ -21,12 +21,12 @@ public class ReservationInputForm {
 	//チェックイン日を取得する
 	public LocalDate getCheckinDate() {
 		String[] checkinDateToCheckoutDate = getFromCheckinDateToCheckoutDate().split("から");
-		return LocalDate.parse(checkinDateToCheckoutDate[0]);
+		return LocalDate.parse(checkinDateToCheckoutDate[0].trim());
 	}
 
 	//チェックアウト日を取得する
 	public LocalDate getCheckoutDate() {
 		String[] checkinDateAndCheckoutDate = getFromCheckinDateToCheckoutDate().split("から");
-		return LocalDate.parse(checkinDateAndCheckoutDate[1]);
+		return LocalDate.parse(checkinDateAndCheckoutDate[1].trim());
 	}
 }
