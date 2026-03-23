98e428fcd6253b9ddfe9f3543ff0515b1d96fdad
sima00rnmr <sima92rnmr@gmail.com>
2026-03-23 14:32:59 +0900

    環境変数読み込みの為再起動（一旦保存）

--- moattravel_3s/src/main/java/com/example/moattravel/controller/ReservationController.java
+++ moattravel_3s/src/main/java/com/example/moattravel/controller/ReservationController.java
@@ -2,6 +2,8 @@ package com.example.moattravel.controller;
 
 import java.time.LocalDate;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -15,7 +17,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.moattravel.entity.House;
@@ -27,18 +28,21 @@ import com.example.moattravel.repository.HouseRepository;
 import com.example.moattravel.repository.ReservationRepository;
 import com.example.moattravel.security.UserDetailsImpl;
 import com.example.moattravel.service.ReservationService;
+import com.example.moattravel.service.StripeService;
 
 @Controller
 public class ReservationController {
 	private final ReservationRepository reservationRepository;
 	private final HouseRepository houseRepository;
 	private final ReservationService reservationService;
+	private final StripeService stripeService;
 
 	public ReservationController(ReservationRepository reservationRepository, HouseRepository houseRepository,
-			ReservationService reservationService) {
+			ReservationService reservationService, StripeService stripeService) {
 		this.reservationRepository = reservationRepository;
 		this.houseRepository = houseRepository;
 		this.reservationService = reservationService;
+		this.stripeService = stripeService;
 	}
 
 	@GetMapping("/reservations")
@@ -81,7 +85,8 @@ public class ReservationController {
 	@GetMapping("/houses/{id}/reservations/confirm")
 	public String confim(@PathVariable(name = "id") Integer id,
 			@ModelAttribute ReservationInputForm reservationInputForm,
-			@AuthenticationPrincipal UserDetailsImpl userDetailImpl, Model model) {
+			@AuthenticationPrincipal UserDetailsImpl userDetailImpl, HttpServletRequest httpServletRequest,
+			Model model) {
 		House house = houseRepository.getReferenceById(id);
 		User user = userDetailImpl.getUser();
 
@@ -96,17 +101,20 @@ public class ReservationController {
 
 		ReservationRegisterForm reservationRegisterForm = new ReservationRegisterForm(house.getId(), user.getId(),
 				checkinDate.toString(), checkoutDate.toString(), reservationInputForm.getNumberOfPeople(), amount);
+		String sessionId = stripeService.createStripeSession(house.getName(), reservationRegisterForm,
+				httpServletRequest);
 
 		model.addAttribute("house", house);
 		model.addAttribute("reservationRegisterForm", reservationRegisterForm);
+		model.addAttribute("sessionId", sessionId);
 		return "reservations/confirm";
 
 	}
-	@PostMapping("/houses/{id}/reservations/create")
+	/*@PostMapping("/houses/{id}/reservations/create")
 	public String create(@ModelAttribute ReservationRegisterForm reservationRegisterForm) {
 		reservationService.create(reservationRegisterForm);
 		
 		return "redirect:/reservations?reserved";
-	}
+	}*/
 
 }
