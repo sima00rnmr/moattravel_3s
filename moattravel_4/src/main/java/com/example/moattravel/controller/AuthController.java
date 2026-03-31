e4be0fb2573366490d00dc35ea0d8f9ebabd4f03
sima00rnmr <sima92rnmr@gmail.com>
2026-03-31 13:55:07 +0900

    22章完了

--- moattravel_4/src/main/java/com/example/moattravel/controller/AuthController.java
+++ moattravel_4/src/main/java/com/example/moattravel/controller/AuthController.java
@@ -15,43 +15,46 @@ import com.example.moattravel.service.UserService;
 
 @Controller
 
-public class AuthController{
+public class AuthController {
 	private final UserService userService;
 	
+
 	public AuthController(UserService userService) {
 		this.userService = userService;
-		
+
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
-		return"auth/login";
+		return "auth/login";
 	}
-	
+
 	@GetMapping("/signup")
 	public String signup(Model model) {
-	model.addAttribute("signupForm",new SignupForm());	
-	return "auth/signup";
+		model.addAttribute("signupForm", new SignupForm());
+		return "auth/signup";
 	}
-	
+
 	@PostMapping("/signup")
-	public String signup(@ModelAttribute @Validated SignupForm signupForm,BindingResult bindingResult,RedirectAttributes redirectAttributes) {
-		
-		  // メールアドレスが登録済みであれば、BindingResultオブジェクトにエラー内容を追加する
-		if(userService.isEmailRegistered(signupForm.getEmail())) {
-			FieldError fieldError = new FieldError(bindingResult.getObjectName(),"email","既に登録済みのアドレスです");
+	public String signup(@ModelAttribute @Validated SignupForm signupForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+
+		// メールアドレスが登録済みであれば、BindingResultオブジェクトにエラー内容を追加する
+		if (userService.isEmailRegistered(signupForm.getEmail())) {
+			FieldError fieldError = new FieldError(bindingResult.getObjectName(), "email", "既に登録済みのアドレスです");
 		}
-		if (!userService.isSamePassword(signupForm.getPassword(),signupForm.getPasswordConfirmation())) {
-			FieldError(bindingResult.getObjectName(),"password","パスワードが一致しません");
+		if (!userService.isSamePassword(signupForm.getPassword(), signupForm.getPasswordConfirmation())) {
+			FieldError fieldError =new FieldError(bindingResult.getObjectName(), "password", "パスワードが一致しません");
 			bindingResult.addError(fieldError);
 		}
-		if(bindingResult.hasErrors()) {
+		if (bindingResult.hasErrors()) {
 			return "auth/signup";
 		}
 		userService.create(signupForm);
-		redirectAttributes.addFieldbute()
-		
-		
+		redirectAttributes.addFlashAttribute("successMessage", "会員登録が完了しました");
+
+		return "redirect:/";
+
 	}
-	
+
 }
