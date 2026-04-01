2b47cc1be461dcbf6d8bead6a192a72d70ebf794
sima00rnmr <sima92rnmr@gmail.com>
2026-04-01 18:05:41 +0900

    18時定時コミット34章途中

---
+++ moattravel_4/src/main/java/com/example/moattravel/form/ReservationRegisterForm.java
@@ -0,0 +1,22 @@
+package com.example.moattravel.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class ReservationRegisterForm {
+	private Integer houseId;
+
+	private Integer usseId;
+
+	private String checkinDate;
+
+	private String checkoutDate;
+
+	private Integer numberOfPeople;
+
+	private Integer amount;
+
+}
