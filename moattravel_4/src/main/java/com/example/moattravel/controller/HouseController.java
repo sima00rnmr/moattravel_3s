cdf4afe194dc7e97592bdafcfcb66947e1e8b79e
sima00rnmr <sima92rnmr@gmail.com>
2026-04-01 17:01:53 +0900

    33章終了

--- moattravel_4/src/main/java/com/example/moattravel/controller/HouseController.java
+++ moattravel_4/src/main/java/com/example/moattravel/controller/HouseController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel.entity.House;
+import com.example.moattravel.form.ReservationInputForm;
 import com.example.moattravel.repository.HouseRepository;
 
 @Controller
@@ -78,6 +79,7 @@ public class HouseController {
 		House house = houseRepository.getReferenceById(id);
 
 		model.addAttribute("house", house);
+		model.addAttribute("reservationInputForm", new ReservationInputForm());
 
 		return "houses/show";
 	}
